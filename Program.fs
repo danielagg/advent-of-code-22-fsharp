f6e00d5d539030780ec9fa071f40a6f694e15c5d
Daniel Agg <daniel.agg@outlook.com>
2023-11-27 16:48:04 +0100

    day1, part2

--- Program.fs
+++ Program.fs
@@ -1,2 +1,3 @@
 ﻿[<EntryPoint>]
 System.Console.WriteLine(Day01.Part1.run())
+System.Console.WriteLine(Day01.Part2.run())
--- day1/solution.fs
+++ day1/solution.fs
@@ -2,16 +2,25 @@ namespace Day01
 
 open System.IO
 
+module private InternalModule =
+    let calculateCaloriesPerElves () =
+        let inputFilePath = Path.Combine(__SOURCE_DIRECTORY__, "input.txt")
+        let perElfInputData = File.ReadAllText(inputFilePath).Split("\n\n")
+
+        Array.map (fun (line: string) ->
+            line.Split("\n")
+            |> Array.map int
+            |> Array.sum
+        ) perElfInputData
+        |> Array.sortDescending
+
 module Part1 =
     let run () =
-        let inputFilePath = Path.Combine(__SOURCE_DIRECTORY__, "input.txt")
-        let perElfInputData = File.ReadAllText(inputFilePath).Split("\n\n");
+        let caloriesPerElves = InternalModule.calculateCaloriesPerElves()
+        caloriesPerElves[0]
 
-        let caloriesPerElves =
-            Array.map (fun (line: string) ->
-                line.Split("\n")
-                |> Array.map int
-                |> Array.sum
-            ) perElfInputData
-
-        Array.max caloriesPerElves
+module Part2 =
+    let run () =
+        let caloriesPerElves = InternalModule.calculateCaloriesPerElves()
+        Array.take 3 caloriesPerElves
+        |> Array.sum
